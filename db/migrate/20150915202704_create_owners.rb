ca0cdf0ceb38e5a39eb6cb9c9c2c8c6947dabd57
Jul Jen <juliajen.prog@gmail.com>
2018-04-11 05:31:41 +0000

    automatically backed up by learn

--- db/migrate/20150915202704_create_owners.rb
+++ db/migrate/20150915202704_create_owners.rb
@@ -1,11 +1,18 @@
 class CreateOwners < ActiveRecord::Migration[4.2]
-  def up
+  def change
     create_table :owners do |t|
       t.string :name
     end
   end
-
-  def down
-    drop_table :owners
-  end
 end
+
+#   def up
+#     create_table :owners do |t|
+#       t.string :name
+#     end
+#   end
+#
+#   def down
+#     drop_table :owners
+#   end
+# end
